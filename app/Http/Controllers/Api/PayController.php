290c2b744b707bd22bd59fbdb496b8dbf8a18ff3
安小游 <471607346@qq.com>
2019-04-26 14:40:30 +0800

    commit

--- app/Http/Controllers/Api/PayController.php
+++ app/Http/Controllers/Api/PayController.php
@@ -10,6 +10,44 @@ use Yansongda\LaravelPay\Facades\Pay;
 class PayController extends ApiBaseController
 {
     //支付
+    /**
+     * @api {POST} pay/:order_id 支付
+     * @apiSampleRequest pay/:order_id
+     * @apiParam {String} type  支付方式(必填【'alipay','wechat-app-pay'】)
+     * @apiHeader {String} authorization Authorization value.
+     * @apiPermission 无
+     * @apiName pay
+     * @apiGroup G-Pay
+     * @apiVersion 1.0.0
+     * @apiDescription   api   支付
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *          "status":"200",
+     *          "msg":"支付链接创建成功",
+     *          "data":[
+     *              "pay_url":"支付链接"
+     *          ]
+     *     }
+     *
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 422 Not Found  422错误提示,请使用ajax异常扑获取
+     *      {
+     *          "message": "The given data was invalid.",
+     *              "errors": {
+     *                   "type": [
+     *                       "type 不能为空。"
+     *                   ]
+     *               }
+     *            }
+     * @apiErrorExample Error-Response:
+     *      {
+     *          "status":"400",
+     *          "msg":"错误提示",
+     *          "data":[]
+     *      }
+     */
     public function pay(PayRequest $request,Order $order)
     {
         $data = $request->all();
