e3eb669234c1f34183231bf0643344841b92a69e
安小游 <471607346@qq.com>
2019-05-09 15:14:12 +0800

    commit

--- app/Http/Resources/Api/UserResource.php
+++ app/Http/Resources/Api/UserResource.php
@@ -16,7 +16,7 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'mobile' => ($this->mobile),
+            'mobile' => substr_replace($this->mobile,'****',3,4),
             'avatar' =>$this->avatar,
             'balance' => $this->balance,
         ];
